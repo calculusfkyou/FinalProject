f1c36139abaf9a1146db5b935b57f5c2408b3321
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2024-01-06 21:37:08 +0800

    Update PlayerInterface.java

--- FinalProject/PlayerInterface.java
+++ FinalProject/PlayerInterface.java
@@ -14,6 +14,7 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -97,8 +98,7 @@ public class PlayerInterface extends JFrame {
 	 * Create the frame.
 	 */
 	public PlayerInterface() {
-		setIconImage(Toolkit.getDefaultToolkit()
-				.getImage("C:\\Users\\GIGABYTE\\eclipse-workspace\\Demo\\src\\Images\\logo.png"));
+		setIconImage(Toolkit.getDefaultToolkit().getImage(PlayerInterface.class.getResource("/Images/logo.png")));
 		setResizable(false); // 不能調整視窗大小
 		setTitle("玩家");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -126,9 +126,7 @@ public class PlayerInterface extends JFrame {
 		chatPanel.setLayout(null);
 		contentPane.add(chatPanel);
 
-		ImageIcon conversation = new ImageIcon(
-				"C:\\Users\\GIGABYTE\\eclipse-workspace\\Demo\\src\\Images\\conversation.png");
-		JLabel conversation_lbl = new JLabel(conversation);
+		JLabel conversation_lbl = new JLabel(new ImageIcon(PlayerInterface.class.getResource("/Images/conversation.png")));
 		conversation_lbl.setBounds(0, -2, 70, 70);
 		conversation_lbl.addMouseListener(new MouseAdapter() {
 			@Override
@@ -150,8 +148,7 @@ public class PlayerInterface extends JFrame {
 		conversation_lbl.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)); // 設定游標為手形
 		chatPanel.add(conversation_lbl);
 
-		ImageIcon quest = new ImageIcon("C:\\Users\\GIGABYTE\\eclipse-workspace\\Demo\\src\\Images\\quest.png");
-		JLabel quest_lbl = new JLabel(quest);
+		JLabel quest_lbl = new JLabel(new ImageIcon(PlayerInterface.class.getResource("/Images/quest.png")));
 		quest_lbl.setBounds(71, -2, 70, 70);
 		quest_lbl.addMouseListener(new MouseAdapter() {
 			@Override
@@ -173,8 +170,7 @@ public class PlayerInterface extends JFrame {
 		quest_lbl.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)); // 設定游標為手形
 		chatPanel.add(quest_lbl);
 
-		ImageIcon bug = new ImageIcon("C:\\Users\\GIGABYTE\\eclipse-workspace\\Demo\\src\\Images\\anti-bug.png");
-		JLabel bug_lbl = new JLabel(bug);
+		JLabel bug_lbl = new JLabel(new ImageIcon(PlayerInterface.class.getResource("/Images/anti-bug.png")));
 		bug_lbl.setBounds(142, -2, 70, 70);
 		bug_lbl.addMouseListener(new MouseAdapter() {
 			@Override
@@ -196,9 +192,7 @@ public class PlayerInterface extends JFrame {
 		bug_lbl.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)); // 設定游標為手形
 		chatPanel.add(bug_lbl);
 
-		ImageIcon customer = new ImageIcon(
-				"C:\\Users\\GIGABYTE\\eclipse-workspace\\Demo\\src\\Images\\customer-service.png");
-		JLabel customer_lbl = new JLabel(customer);
+		JLabel customer_lbl = new JLabel(new ImageIcon(PlayerInterface.class.getResource("/Images/customer-service.png")));
 		customer_lbl.setBounds(213, -2, 70, 70);
 		customer_lbl.addMouseListener(new MouseAdapter() {
 			@Override
@@ -290,8 +284,7 @@ public class PlayerInterface extends JFrame {
 		separator2.setBounds(68, 0, 1, 69);
 		IDPanel.add(separator2);
 
-		ImageIcon avatar = new ImageIcon("C:\\Users\\GIGABYTE\\eclipse-workspace\\Demo\\src\\Images\\avatar.jpg");
-		JLabel avatar_lbl = new JLabel(avatar);
+		JLabel avatar_lbl = new JLabel(new ImageIcon(PlayerInterface.class.getResource("/Images/avatar.jpg")));
 		avatar_lbl.setBounds(3, 2, 65, 65);
 		IDPanel.add(avatar_lbl);
 
@@ -416,8 +409,7 @@ public class PlayerInterface extends JFrame {
 		allianceManage_btn.setBounds(400, 40, 100, 40);
 		contentPane.add(allianceManage_btn);
 
-		ImageIcon BkIcon = new ImageIcon(
-				"C:\\Users\\GIGABYTE\\eclipse-workspace\\Demo\\src\\Images\\buildAllianceBk.jpg"); // 背景圖片
+		ImageIcon BkIcon = new ImageIcon(PlayerInterface.class.getResource("/Images/buildAllianceBk.jpg")); // 背景圖片
 		Image originalImage = BkIcon.getImage();
 		float transparency = 0.5f; // 設定透明度，0.0 是完全透明，1.0 是完全不透明
 		Image transparentImage = makeTransparent(originalImage, transparency); // 創建具有指定透明度的新圖像
@@ -471,12 +463,12 @@ public class PlayerInterface extends JFrame {
 	private void initializeAnnouncements() {
 		// 初始化布告欄消息和相應的圖片
 		announcements = new ArrayList<>();
-		announcements.add(new Announcement("羅傑這咖都鑽石了，這裡誰還卡在翡翠？？",
-				"C:\\Users\\GIGABYTE\\eclipse-workspace\\Demo\\src\\Images\\news8.jpeg"));
+		announcements.add(new Announcement("羅傑這咖都鑽石了，這裡誰還卡在翡翠？？",PlayerInterface.class.getResource("/Images/news8.jpeg")
+				));
 		announcements.add(new Announcement("這咖的犽凝跟熊班長萎縮的腎一樣小 真的點點點",
-				"C:\\Users\\GIGABYTE\\eclipse-workspace\\Demo\\src\\Images\\news9.jpg"));
+				PlayerInterface.class.getResource("/Images/news98.jpg")));
 		announcements.add(new Announcement("世界八強AD 單腎貴族 被路人珍娜當狗在噴...",
-				"C:\\Users\\GIGABYTE\\eclipse-workspace\\Demo\\src\\Images\\news10.jpg"));
+				PlayerInterface.class.getResource("/Images/news10.jpg")));
 		// 可以加入更多布告欄消息和相應的圖片
 
 		// 隨機排序，以便輪播效果
@@ -517,18 +509,18 @@ public class PlayerInterface extends JFrame {
 	// 內部類別，表示一條布告欄消息和相應的圖片
 	private static class Announcement {
 		private String message;
-		private String imagePath;
+		private URL imagePath;
 
-		public Announcement(String message, String imagePath) {
+		public Announcement(String message, URL url) {
 			this.message = message;
-			this.imagePath = imagePath;
+			this.imagePath = url;
 		}
 
 		public String getMessage() {
 			return message;
 		}
 
-		public String getImagePath() {
+		public URL getImagePath() {
 			return imagePath;
 		}
 	}
